36bea227f78e8a8e12bc9b99ec3e535f0db49600
Francisco <149792958+fbarbosa2@users.noreply.github.com>
2025-10-22 19:42:41 +0100

    created basic fil structure and functions to delete and edit read from array books

---
+++ scripts.js
@@ -0,0 +1,87 @@
+const myLibrary = [];
+const tableDOM = document.getElementById("bookTable");
+const bookBtnDOM = document.getElementById("book-btn");
+
+function Book(id, title, author, pages){
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+} 
+
+function addBookToLibrary(title, author, pages){
+    const id = crypto.randomUUID();
+    const book = new Book(id, title, author, pages);
+    myLibrary.push(book);
+}
+function deleteBookFromLibrary(id){
+    myLibrary.forEach(book =>{
+        if(book.id === id){
+            myLibrary.pop(book);
+        }
+    })
+    console.log(`array: ${myLibrary}`);
+}
+function readBook(id){
+    myLibrary.forEach(book => {
+        if(book.id === id){
+            if(book.read){
+                book.read = false;
+            } else {
+                book.read = true;
+            }
+        }
+    })
+}
+
+addBookToLibrary("livro1", "author1", 210);
+addBookToLibrary("livro2", "author2", 220);
+addBookToLibrary("livro3", "author3", 230);
+addBookToLibrary("livro4", "author4", 240);
+
+function renderTable(){
+
+    myLibrary.forEach(book => {
+        const row = document.createElement("tr");
+        const bookProp = Object.getOwnPropertyNames(book);
+        bookProp.forEach(prop => {
+            const propTD = document.createElement("td");
+            propTD.textContent = book[prop];
+            row.appendChild(propTD);
+        });
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete Book";
+        deleteBtn.dataset.bookId = book.id;
+        deleteBtn.addEventListener("click", (e) => {
+            const idToDelete = e.target.dataset.bookId;
+            deleteBookFromLibrary(idToDelete);
+        });
+        const deleteCell = document.createElement("td");
+        deleteCell.appendChild(deleteBtn);
+        row.appendChild(deleteCell);
+
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Read";
+        editBtn.dataset.bookId = book.id;
+        editBtn.addEventListener("click", (e) => {
+            const idToEdit = e.target.dataset.bookId;
+            readBook(idToEdit);
+            console.log(`book readed: ${book.read}`);
+        });
+        row.appendChild(editBtn);
+
+        tableDOM.appendChild(row);
+    });
+
+    // bookBtnDOM.addEventListener("click", () => {
+
+    // })
+
+}
+
+renderTable();
